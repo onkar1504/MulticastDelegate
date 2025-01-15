71add27ba72f7924e51a36bd1fe04adf84833313
onkar lonkar <onkarlonkar55@gmail.com>
2025-01-16 00:02:07 +0530

    Checking By Onkar

--- MulticastDelegate/Program.cs
+++ MulticastDelegate/Program.cs
@@ -1,10 +1,46 @@
 ﻿namespace MulticastDelegate
 {
-    internal class Program
+    public delegate void Sampledelegate();
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //instance of delegate
+            //Sampledelegate DelObj = new Sampledelegate(SampleMethod1);
+            //DelObj();
+
+            //Cretae Multiple Instance Of delgates
+            Sampledelegate Del1,Del2,Del3,Del4;
+            Del1 = new Sampledelegate(SampleMethod1);  // pointing to methods
+            Del2 = new Sampledelegate(SampleMethod2);
+            Del3 = new Sampledelegate(SampleMethod3);
+
+            Del4 = Del1 + Del2 + Del3;      // adding all three delgates togethers
+            Del4();
+            Console.WriteLine("\n");
+
+            // ANother Example
+            Sampledelegate Del = new Sampledelegate(SampleMethod1);
+            Del += SampleMethod2;
+            Del += SampleMethod3;
+            Del();
+
+            Console.ReadLine();
         }
+        public static void SampleMethod1()
+        {
+            Console.WriteLine("SampleMethod1 Invoked");
+        }
+        public static void SampleMethod2()
+        {
+            Console.WriteLine("SampleMethod2 Invoked");
+
+        }
+        public static void SampleMethod3()
+        {
+            Console.WriteLine("SampleMethod3 Invoked");
+        }
+
+
     }
 }
---
+++ MulticasteDelegates2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MulticastDelegate
+{
+    public delegate int Sampledelegate();
+    public delegate void SampleDelegate2(out int integer);
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //instance of delegate
+            Sampledelegate Del = new Sampledelegate(SampleMethod1);
+            Del += SampleMethod2;
+            int DelgateReturnValue = Del();
+            Console.WriteLine(DelgateReturnValue);
+            Console.WriteLine("\n");
+
+            // another Example
+            SampleDelegate2 Del2 = new SampleDelegate2(SampleMethod3);
+            Del2 += SampleMethod4;
+
+            int DelegateReturnValuewithOutparameter = -1;
+            Console.WriteLine("DelegateReturnValuewithOutparameter = {0}", DelegateReturnValuewithOutparameter);
+            Console.ReadLine();
+        }
+        public static int SampleMethod1()
+        {
+            return 1;
+        }
+        public static int SampleMethod2()
+        {
+            return 2;
+        }
+
+        //By using Out parameters
+        public static void SampleMethod3(out int Number)
+        {
+            Number = -1;
+        }
+        public static void SampleMethod4(out int Number)
+        {
+            Number = -2;
+        }
+
+
+    }
+}
