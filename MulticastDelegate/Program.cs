17f87c59943bc4aad92f216d2535d51e9cdc7a21
onkar lonkar <onkarlonkar55@gmail.com>
2025-01-15 23:05:45 +0530

    Add project files.

---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MulticastDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
